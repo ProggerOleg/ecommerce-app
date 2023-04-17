38d406748c86d6819b5afe464589e87b9d700dae
ProggerOleg <olegtar25@gmail.com>
2023-04-17 17:21:09 +0300

    headers done

---
+++ src/components/Header.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
+
+function Header() {
+  return (
+    <>
+      <header className="header-top-strip">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free Shipping Over $100 & Free Returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a className="text-white" href="tel:+38 099 250 67 01">
+                  +38 099 250 67 01
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h2>
+                <Link className="text-white">DevCorner.</Link>
+              </h2>
+            </div>
+            <div className="col-5">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search Product Here..."
+                  aria-label="Search Product Here..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6" />
+                </span>
+              </div>
+            </div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link>
+                    <img src="/images/compare.svg" alt=""></img>
+                    <p>
+                      Compare <br /> Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link>
+                    <img src="/images/wishlist.svg" alt="wishlist"></img>
+                    <p>
+                      Favourite <br /> Wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link>
+                    <img src="/images/user.svg" alt="user"></img>
+                    <p>
+                      Login <br /> My Account
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link>
+                    <img src="/images/cart.svg" alt="cart"></img>
+                    <div className="d-flex flex-column">
+                      <span className="badge bg-white text-dark">0</span>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
